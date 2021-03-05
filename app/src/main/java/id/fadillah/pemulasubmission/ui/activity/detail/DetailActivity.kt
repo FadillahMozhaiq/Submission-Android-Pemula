580838c69db022ee6d541a67c1e6a0f4aa8f33fd
Fadillah Mozhaiq <mozhaiqramadhani05@gmail.com>
2021-03-05 10:37:01 +0800

    Add Layout Detail

--- app/src/main/java/id/fadillah/pemulasubmission/ui/activity/detail/DetailActivity.kt
+++ app/src/main/java/id/fadillah/pemulasubmission/ui/activity/detail/DetailActivity.kt
@@ -2,21 +2,22 @@ package id.fadillah.pemulasubmission.ui.activity.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import id.fadillah.pemulasubmission.R
+import id.fadillah.pemulasubmission.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_ENDPOINT = "extra_endpoint"
     }
 
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
-        findViewById<FloatingActionButton>(R.id.fab_bookmark).setOnClickListener { view ->
+        binding.fabBookmark.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
