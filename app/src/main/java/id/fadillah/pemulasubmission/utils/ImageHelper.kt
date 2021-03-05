ebb395a0cee5b4f84100aa4f2ba66287b8e8fddf
Fadillah Mozhaiq <mozhaiqramadhani05@gmail.com>
2021-03-05 09:38:06 +0800

    Fix Image Helper

--- app/src/main/java/id/fadillah/pemulasubmission/utils/ImageHelper.kt
+++ app/src/main/java/id/fadillah/pemulasubmission/utils/ImageHelper.kt
@@ -7,6 +7,7 @@ import com.squareup.picasso.Picasso
 import id.fadillah.pemulasubmission.R
 
 object ImageHelper {
+    private val TAG = ImageHelper::class.java.simpleName
     fun getImage(
         imageView: ImageView,
         url: String,
@@ -26,4 +27,40 @@ object ImageHelper {
                 }
             })
     }
+//    fun getImage(
+//        imageView: ImageView,
+//        url: String,
+//        placeholder: Int = R.drawable.placeholder,
+//        error: Int = R.drawable.ic_no_images
+//    ) {
+//        Glide
+//            .with(imageView.context)
+//            .load(url)
+//            .listener(object : RequestListener<Drawable> {
+//                override fun onLoadFailed(
+//                    e: GlideException?,
+//                    model: Any?,
+//                    target: Target<Drawable>?,
+//                    isFirstResource: Boolean
+//                ): Boolean {
+//                    Log.e(TAG, "Error Load Image: ${e?.message}")
+//                    return false
+//                }
+//
+//                override fun onResourceReady(
+//                    resource: Drawable?,
+//                    model: Any?,
+//                    target: Target<Drawable>?,
+//                    dataSource: DataSource?,
+//                    isFirstResource: Boolean
+//                ): Boolean {
+//                    Log.d(TAG, "Load Image Success")
+//                    return false
+//                }
+//
+//            })
+//            .placeholder(placeholder)
+//            .error(error)
+//            .into(imageView)
+//    }
 }
